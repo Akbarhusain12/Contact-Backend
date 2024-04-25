06cd42d7b977b773c8d1005ef4b71d2360f69584
Your Name <akbarhusainkadiwala32@gmail.com>
2024-04-25 16:02:09 +0530

    added mocha and chai for testing the application

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config({ path: './.env' });
 
 const port = process.env.PORT || 5000;
 
-connectDB()
+// connectDB()
 app.use(express.json())
 app.use('/api/contact', contactRoutes);
 app.use('/api/users', userRoutes);
@@ -20,3 +20,5 @@ app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app
